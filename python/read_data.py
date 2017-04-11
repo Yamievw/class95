dcf49aec6a7afa5c631a783b1b6f480f71693195
RobDHess <HesselinkRD@gmail.com>
2017-04-11 15:18:34 +0200

    Create read_data.py
    Een file die alle data gaat inlezen.

---
+++ python/read_data.py
@@ -0,0 +1,46 @@
+# This file reads in the data and fills two lists: one with 
+# students and one with courses. This data can be readily used
+# by importing this module. 
+
+import csv
+
+###
+### Hier moet de course-class
+###
+
+
+class student():
+    def __init__(self, name, surname, number, courses):
+        self.name = name
+        self.surname = surname
+        self.number = number
+        self.courses = []
+        
+        
+        for course in courses:
+            if course != '':
+                if course == 'Zoeken':
+                    self.courses.append('Zoeken, sturen en bewegen')
+                elif course != ' sturen en bewegen':
+                    self.courses.append(course)
+        self.courses = sorted(self.courses)
+        self.coursenumber = len(self.courses)
+
+    def get_courses(self):
+        return self.courses
+    def get_coursenumber(self):
+        return self.coursenumber
+
+    
+students = []
+courses = []
+
+with open('studenten_roostering.CSV', 'rb') as csvfile:
+    # open txt file. 
+    reader = csv.reader(csvfile, delimiter = ",")
+    for row in reader:
+        # create students. 
+        students.append(student(row[1], row[0], row[2], row[3:]))
+
+
+        
