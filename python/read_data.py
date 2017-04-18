c46a0311d0efe87f2d3b48a55867491639ed8c8d
RobDHess <HesselinkRD@gmail.com>
2017-04-18 13:33:48 +0200

    Update read_data.py
    We can now read in courses yay!

--- python/read_data.py
+++ python/read_data.py
@@ -4,9 +4,30 @@
 
 import csv
 
-###
-### Hier moet de course-class
-###
+class Courses():
+    def __init__(self, name, components, registrants):
+        self.name = name
+        self.components = components # a dictionary of yet unkown format. 
+        self.registrants = registrants # a list containing students in an unknown format. 
+           
+        self.registrants = sorted(self.registrants) # is dit nodig of niet? hangt van format registrants af. 
+        self.amount_registrants = len(self.registrants)
+
+    def get_registrants(self):
+        return self.registrants
+    def get_amount_registrants(self):
+        return self.amount_registrants
+    def get_lectures(self):
+        return self.components["lectures"]
+    def get_tutorials(self):
+        return self.components["tutorials"]
+    def get_labs(self):
+        return self.components["labs"]
+    def get_name(self):
+        return self.name
+    
+    def add_registrant(self, registrant):
+        self.registrants.append(registrant)
 
 
 class Student():
@@ -32,15 +53,33 @@ class Student():
         return self.coursenumber
 
     
-students = []
-courses = []
+students = {}
+courses = {}
+
+with open('vakken.CSV', 'rb') as csvfile:
+    reader = csv.reader(csvfile, delimiter = ",")
+    for row in reader:
+        components = {}
+        components["lectures"] = row[1]
+        components["tutorials"] = (row[2], row[3]) # misschien problemen met nvt
+        components["labs"] = (row[4], row[5])
+        
+        # create course object. 
+        courses[row[0]] = Courses(row[0], components, [])
+          
 
 with open('studenten_roostering.CSV', 'rb') as csvfile:
     # open txt file. 
     reader = csv.reader(csvfile, delimiter = ",")
     for row in reader:
         # create students. 
-        students.append(Student(row[1], row[0], row[2], row[3:]))
-
+        students[row[2]] = Student(row[1], row[0], row[2], row[3:])
+        for course in row[3:]:
+            try:
+                courses[course].add_registrant(row[2])
+            except KeyError:
+                continue
+        
+        
 
         
