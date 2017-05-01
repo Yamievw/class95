1ae0de6f4107b5d901c8802df1f2db6b3e0c2afa
MelissaMW2 <melissa.fsr.feb@gmail.com>
2017-05-01 13:40:09 +0200

    Updated Schedule
    Added: str self activities

--- python/schedule.py
+++ python/schedule.py
@@ -27,7 +27,10 @@ class Activity():
         return self.participants
 
     def add_participants(self, participants):
-        self.participants.append(participants) # sanity check iemand twee keer.
+        self.participants.append(participants) # sanity check iemand twee keer. 
+
+    def __str__(self):
+        return self.name, self.type, self.capacity, self.room, self.participants
 
 class Schedule():
     # a 5x5 matrix that represents the available timeslots
@@ -39,7 +42,6 @@ class Schedule():
         self.timetable = [[[] for x in range(5)] for y in range(5)]
         self.day_dict = {0:"Monday", 1:"Tuesday", 2:"Wednesday", 3:"Thursday", 4:"Friday"}
         self.timeslot_dict = {0:"9:00-11:00", 1:"11:00-13:00", 2:"13:00-15:00", 3:"15:00-17:00", 4:"17:00-19:00"}
-                
         
 
     def get_timetable(self):
@@ -53,7 +55,7 @@ class Schedule():
             i += i
         return 0 # if unsuccessful
     def update_timetable(self, new_timetable):
-        self.timetable = new_timetable # sanity check hiero moet noggg!
+        self.timetable = new_timetable # sanity check hiero moet noggg! #check of het een 5 bij 5 matrix is!!
         return self
 
     def add_activity(self, day, timeslot, activity):
@@ -71,11 +73,3 @@ class Schedule():
                 for activity in self.timetable[timeslot][day]:
                     print activity.name + " " + activity.type
         return "--"
-                    
-        
-    
-
-
-    
-        
-        
