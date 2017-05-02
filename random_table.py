1ed058418d61a8f469a3e64b212813f776640ec3
MelissaMW2 <melissa.fsr.feb@gmail.com>
2017-05-02 20:09:37 +0200

    Random table mét studenten!

---
+++ random_table.py
@@ -0,0 +1,59 @@
+# creates a randomized timetable, with students.
+
+from read_data import *
+from schedule import *
+
+import random
+
+def random_table():
+    courses_test = courses.values()
+    activities = []
+    test = Schedule()
+
+    for course in courses_test:
+        components = course.get_components()
+        for key in components:
+            for i in range(components[key][0]):
+                name = course.get_name()
+                ttype = key
+                capacity = components[key][1]
+
+                activities.append(Activity(name, ttype, capacity))
+
+    for activity in activities:
+        i = random.randint(0, 4)
+        j = random.randint(0, 3)  # no evening timeslot.
+        test.add_activity(i, j, activity)
+
+    test = add_students(test)
+
+    return test
+
+def add_students(schedule):
+    timetable = schedule.timetable
+    for day in range(5):
+        for timeslot in range(5):
+            for activity in timetable[timeslot][day]:
+                if activity.type == "lectures":
+                    activity.add_participants(courses[activity.name].get_registrants())
+                else:
+                    activiteit = make_groups(activity)
+
+def make_groups(activity):
+    data = []
+    students = courses[activity.name].get_registrants()
+    for student in students:
+        data.append(student)
+
+    maxsize = int(activity.get_capacity())
+
+    n = math.ceil(float(len(data))/(maxsize))
+    n = int(n)
+
+    groups = [data[i::n] for i in range(n)]
+                 
+    for i, group in enumerate(groups):
+        name = activity.name + " (" + str(i + 1) + ")"
+        new_activity = Activity(name, activity.type, activity.capacity, participants=group)
+
+
