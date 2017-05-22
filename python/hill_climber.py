c0c3d40965d403a9b28ab6d6dbdff5e3eb70d16d
MelissaMW2 <melissa.fsr.feb@gmail.com>
2017-05-22 15:10:46 +0200

    HC incl. studenten en rooms + test voor studenten

--- python/hill_climber.py
+++ python/hill_climber.py
@@ -23,5 +23,32 @@ def hill_climber_activities(schedule, iterations=1):
 
     return schedule
 
+def hill_climber_students(schedule, iterations=1):
+    
+    score = schedule.score()
+    for i in range(iterations):
+        new_schedule = swap_student(schedule)
+        new_score = new_schedule.score()
 
+        if new_score > score:
+            score = new_score
+            schedule = new_schedule
+        else:
+            table = schedule.get_timetable()
 
+    return schedule
+
+def hill_climber_rooms(schedule, iterations=1):
+    
+    score = schedule.score()
+    for i in range(iterations):
+        new_schedule = swap_rooms(schedule)
+        new_score = new_schedule.score()
+
+        if new_score > score:
+            score = new_score
+            schedule = new_schedule
+        else:
+            table = schedule.get_timetable()
+
+    return schedule
---
+++ python/hill_climber_teststudents.py
@@ -0,0 +1,21 @@
+from hill_climber import *
+from random_table import *
+
+import matplotlib.pyplot as plt
+# import seaborn as sns
+
+iterations = []
+scores = []
+
+schedule = random_table()
+
+for i in range(10):
+    iterations.append(i)
+    scores.append(schedule.score())
+    schedule = hill_climber_students(schedule)
+
+plt.plot(iterations, scores)
+plt.xlabel("Iterations")
+plt.ylabel("Score")
+plt.title("Hill Climber Students")
+plt.show()
