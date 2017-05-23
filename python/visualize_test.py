af42c865df44c0b02e90d415dea31f46a11ceb29
RobDHess <HesselinkRD@gmail.com>
2017-05-23 15:37:54 +0200

    eveningmaluspng

--- python/visualize_test.py
+++ python/visualize_test.py
@@ -3,9 +3,9 @@ from hill_climber import *
 
 
 schedule = random_table()
-#schedule.plot()
+schedule.plot()
 #schedule.plot_course("Compilerbouw")
-schedule.personal("13284244", True)
+#schedule.personal("13284244", True)
 
 
 #schedule = hill_climber_activities(schedule, 30)
