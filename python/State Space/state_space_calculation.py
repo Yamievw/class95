c4e6f771f597dccd5814f0493dd924eb3a109610
RobDHess <HesselinkRD@gmail.com>
2017-06-01 22:55:54 +0200

    state space

--- python/State Space/state_space_calculation.py
+++ python/State Space/state_space_calculation.py
@@ -1,41 +1,44 @@
 import math
 
-from .read_data import *
-from .random_table import *
+##from read_data import *
+##from random_table import *
+##
+##def stirling(n, k):
+##    output = 0
+##    for j in range(k + 1):
+##        factor = (-1)**(k-j)
+##        output += factor*choose(k, j)*j**n
+##    return output/float(math.factorial(k))
+##
+##def choose(n, m):
+##    output = math.factorial(n)
+##    output = output/float(math.factorial(m)*math.factorial(n-m))
+##    return output
+##
+##def bell(n, l):
+##    output = 0
+##    for k in range(l + 1):
+##        output += stirling(n, k)
+##    return output
+##        
+##
+##
+##possibilities = 0    
+##for course in courses.values():
+##    
+##    n = len(course.registrants)
+##    k = course.no_groups
+##
+##    if k == 0:
+##        continue
+##
+##    possibilities += stirling(n, k)
+##    print course.name
 
-def stirling(n, k):
-    output = 0
-    for j in range(k + 1):
-        factor = (-1)**(k-j)
-        output += factor*choose(k, j)*j**n
-    return output/float(math.factorial(k))
+##print possibilities
+print 25.**129
+print 7.**129
 
-def choose(n, m):
-    output = math.factorial(n)
-    output = output/float(math.factorial(m)*math.factorial(n-m))
-    return output
-
-def bell(n, l):
-    output = 0
-    for k in range(l + 1):
-        output += stirling(n, k)
-    return output
-        
-
-
-possibilities = 0    
-for course in courses.values():
-    
-    n = len(course.registrants)
-    k = course.no_groups
-
-    if k == 0:
-        continue
-
-    possibilities += stirling(n, k)
-    print course.name
-
-print possibilities
-
-print bell(129, 25)
-print bell(129, 7)
+##
+##print bell(129, 25)
+##print bell(129, 7)
